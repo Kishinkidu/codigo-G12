1f5b025f56c90d9c0bc85d373086affa3b995477
Kishinkidu <erick.revoredo58@gmail.com>
2023-02-23 21:38:35 -0500

    final

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
+// Esta es formar en la que JS accede a los elementos
+
+// es el div donde estan todos nuestros cards
+// recordemos que el div con la clase challenge tiene el scroll x (horizontal)
 const challenges = document.querySelector(".challenges");
+
+// obtener los botones de next y back
 const btnNext = document.querySelector(".btn-next");
 const btnBack = document.querySelector(".btn-back");
 
 btnNext.onclick = function () {
+  // Esto quiere decir que cuando hagamos un click en el boton con clase .btn-bnext
+  // hara lo siguiente
+  let slider = challenges.offsetWidth / 4;
+
+  if (challenges.offsetWidth <= 480) {
+    slider = challenges.offsetWidth;
+  }
+
+  // challenges.offsetWidth => nos da el ancho del div challenges
   challenges.scrollBy({
-    left: 50,
+    left: slider +42,
   });
 };
 
 btnBack.onclick = function () {
+  let slider = (challenges.offsetWidth / 4) * -1;
+
+  if (challenges.offsetWidth <= 480) {
+    slider = -challenges.offsetWidth;
+  }
+
   challenges.scrollBy({
-    left: -50,
+    left: slider +42,
   });
 };
